1867f8b07d4beb7889a1dbba27bc932dc842e5c9
mohamedisakr <mohamedisakr@gmail.com>
2020-02-16 12:04:39 +0200

    update LoopingExercisePart03 to check if github working fine

--- LoopingExercisePart03.js
+++ LoopingExercisePart03.js
@@ -9,20 +9,11 @@ suspects = [
   }
 ];
 
-// const colors = suspects.map(suspect => suspect.color);
-// console.log(colors);
+// if there are just 2 elements in the array
 console.log(suspects.length);
 const [{ color: firstColor }, { color: secondColor }] = suspects;
 console.log(firstColor, secondColor);
 
-//   {
-//   const [orange, red] = suspect.color;
-//   console.log(orange, red);
-// });
-
-// const item = {
-//   name: "Miss Scarlet",
-//   color: "red"
-// };
-// const { color } = item;
-// console.log(color);
+// if there are more than 2 elements in the array
+// const colors = suspects.map(suspect => suspect.color);
+// console.log(colors);
